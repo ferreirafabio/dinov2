ed831b83e7909019f8dfe143d1fea9eeae3f90f6
ferreirafabio <ferreirafabio@users.noreply.github.com>
2023-08-09 16:27:59 +0200

    vitg14

--- scripts/exec_datasets_x_model_args.sh
+++ scripts/exec_datasets_x_model_args.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 #SBATCH -p bosch_gpu-rtx2080
-#SBATCH --job-name dinov2_meta_album_vitl14_lora
-#SBATCH -o experiments/metaalbum/vitl14_timestamp/logs/%A-%a.%x.o
-#SBATCH -e experiments/metaalbum/vitl14_timestamp/logs/%A-%a.%x.e
+#SBATCH --job-name dinov2_meta_album_vitg14_lora
+#SBATCH -o experiments/metaalbum/vitg14_lora/logs/%A-%a.%x.out
+#SBATCH -e experiments/metaalbum/vitg14_lora/logs/%A-%a.%x.err
 #SBATCH --gres=gpu:1
 #SBATCH -t 20:00:00
 #SBATCH --array 1-30%20
@@ -13,7 +12,7 @@
 source ~/.profile
 conda activate dinov2
 
-ARGS_FILE=experiments/vitl14_linear_meta_album_lora.args
+ARGS_FILE=experiments/vitg14_linear_meta_album_lora.args
 TASK_SPECIFIC_ARGS=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $ARGS_FILE)
 
 echo $TASK_SPECIFIC_ARGS
